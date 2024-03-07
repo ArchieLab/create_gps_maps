7d2a9622636a61190b54e9f90704a930a940f3ec
Jake Gordon <jacob.b.gordon@gmail.com>
2024-03-07 11:55:47 -0500

    Rewrite query to fetch all the current SWERB locations

--- code/babase_queries/locations.sql
+++ code/babase_queries/locations.sql
@@ -1,10 +1,40 @@
--- Download data from swerb regading groves
+-- Get data about groves & water holes from the related SWERB tables
 
-SELECT swerb_gws.type, swerb_gws.loc, swerb_gw_loc_data.date, swerb_gw_loc_data.xysource, type, altname, 
-       start, finish,
-       swerb_gw_locs.x, swerb_gw_locs.y, swerb_gw_loc_data.notes
-FROM swerb_gw_loc_data
-INNER JOIN swerb_gws
-ON swerb_gws.loc = swerb_gw_loc_data.loc
-INNER JOIN swerb_gw_locs
-ON swerb_gw_locs.loc = swerb_gw_loc_data.loc AND swerb_gw_locs.date = swerb_gw_loc_data.date;
+with max_locs AS (
+                  -- Get only the most-recent coordinates, for locs that have
+                  -- more than one row in SWERB_GW_LOCS
+                  SELECT loc
+                        , max(date::text
+                                || ' '
+                                || coalesce(time::text, '00:00:00'::text)
+                              ) as maxdate
+                    FROM swerb_gw_locs
+                    GROUP BY loc
+                  )
+   , these_locs AS (SELECT swerb_gw_locs.*
+                      FROM swerb_gw_locs
+                      JOIN max_locs
+                        ON max_locs.loc = swerb_gw_locs.loc
+                           AND (swerb_gw_locs.date::text
+                                  || ' '
+                                  || coalesce(swerb_gw_locs.time::text
+                                              , '00:00:00'::text)
+                                ) = max_locs.maxdate
+                    )
+SELECT swerb_gws.type
+     , swerb_gws.loc
+     , these_locs.date
+     , these_locs.xysource
+-- David's original code had two "type" columns, so...okay
+, swerb_gws.type
+     , swerb_gws.altname
+     , swerb_gws.start
+     , swerb_gws.finish
+     , these_locs.x
+     , these_locs.y
+     , these_locs.notes
+  FROM these_locs
+  JOIN swerb_gws
+    ON swerb_gws.loc = these_locs.loc
+  WHERE swerb_gws.finish IS NULL
+    AND swerb_gws.type IN ('G', 'W');
